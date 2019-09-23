9febc5a75f69982b86b657399c055e0e9e5b50e2
Yury Aniskou <klaus24128@gmail.com>
2019-09-23 16:40:28 +0300

    UINV-65 refactoring

--- src/common/utils/getApproveErrorMessage.js
+++ src/common/utils/getApproveErrorMessage.js
@@ -3,7 +3,6 @@ export const APPROVE_ERROR_CODES = {
   FundDistributionsNotPresent: 'FundDistributionsNotPresent',
 };
 
-// eslint-disable-next-line import/prefer-default-export
 export const getApproveErrorMessage = (errorCode) => {
   return APPROVE_ERROR_CODES[errorCode]
     ? `ui-invoice.invoice.actions.approve.error.${APPROVE_ERROR_CODES[errorCode]}`
