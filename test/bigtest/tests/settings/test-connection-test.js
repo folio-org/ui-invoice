7b1ab50c1f324be1cb404306514e959bb72d6676
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2020-04-09 16:52:03 +0300

    add coverage

--- test/bigtest/tests/settings/test-connection-test.js
+++ test/bigtest/tests/settings/test-connection-test.js
@@ -1,6 +1,8 @@
 import { describe, beforeEach, it } from '@bigtest/mocha';
 import { expect } from 'chai';
+import { Response } from 'miragejs';
 
+import { EXPORT_CONFIGURATIONS_API } from '../../../../src/common/constants';
 import setupApplication from '../../helpers/setup-application';
 import BatchGroupConfigurationSettingsInteractor from '../../interactors/BatchGroupConfigurationSettingsInteractor';
 
@@ -32,6 +34,20 @@ describe('Test connection', function () {
 
   describe('Click test connection', () => {
     beforeEach(async function () {
+      await page.testConnectionBtn.click();
+    });
+
+    it('nothing', () => {
+      expect(page.testConnectionBtn.isPresent).to.be.true;
+    });
+  });
+
+  describe('Click test connection with error response', () => {
+    beforeEach(async function () {
+      this.server.post(
+        `${EXPORT_CONFIGURATIONS_API}/:id/credentials/test`,
+        () => new Response(500, { errors: [{cause: 'some error'}] }),
+      );
       await page.testConnectionBtn.click();
     });
 
