d034101baf03a5ce5091444107d9b976a9b3d4c9
Andrei Shumski <Andrei_Shumski2@epam.com>
2020-09-17 14:22:49 +0300

    translate currency

--- src/common/components/Currency/CurrencyValue.js
+++ src/common/components/Currency/CurrencyValue.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
+import { useIntl } from 'react-intl';
 
 import { KeyValue } from '@folio/stripes/components';
 
-const CurrencyValue = ({ value }) => (
-  <KeyValue
-    label={<FormattedMessage id="ui-invoice.invoice.currency" />}
-    value={value}
-  />
-);
+const CurrencyValue = ({ value }) => {
+  const intl = useIntl();
+
+  return (
+    <KeyValue
+      label={intl.formatMessage({ id: 'ui-invoice.invoice.currency' })}
+      value={intl.formatDisplayName(value, { type: 'currency' })}
+    />
+  );
+};
 
 CurrencyValue.propTypes = {
   value: PropTypes.string.isRequired,
