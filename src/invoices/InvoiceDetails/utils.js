994c650625f370685325e3a6ca04ffbcffd4c1dc
Andrei Shumski <Andrei_Shumski2@epam.com>
2020-10-27 10:41:29 +0300

    UINV-198 Error msg: Budget Expense Class not found

--- src/invoices/InvoiceDetails/utils.js
+++ src/invoices/InvoiceDetails/utils.js
@@ -1,6 +1,11 @@
 import { find, get } from 'lodash';
 
-import { INVOICE_STATUS } from '../../common/constants';
+import { EXPENSE_CLASSES_API } from '@folio/stripes-acq-components';
+
+import {
+  INVOICE_STATUS,
+  ERROR_CODES,
+} from '../../common/constants';
 
 // eslint-disable-next-line import/prefer-default-export
 export const createInvoiceLineFromPOL = (poLine, invoiceId, vendor) => {
@@ -34,4 +39,70 @@ export const createInvoiceLineFromPOL = (poLine, invoiceId, vendor) => {
     subTotal: poLine?.cost?.poLineEstimatedPrice || 0,
     ...optionalProps,
   };
+};
+
+export const showUpdateInvoiceError = async (
+  response,
+  showCallout,
+  action,
+  defaultErrorMessageId,
+  expenseClassMutator,
+) => {
+  let error;
+
+  try {
+    error = await response.clone().json();
+  } catch (parsingException) {
+    error = response;
+  }
+
+  const errorCode = error?.errors?.[0]?.code;
+  const code = ERROR_CODES[errorCode];
+
+  switch (code) {
+    case ERROR_CODES.accountingCodeNotPresent:
+    case ERROR_CODES.voucherNumberPrefixNotAlpha:
+    case ERROR_CODES.fundDistributionsNotPresent:
+    case ERROR_CODES.poLineUpdateFailure:
+    case ERROR_CODES.fundCannotBePaid:
+    case ERROR_CODES.transactionCreationFailure: {
+      showCallout({
+        messageId: `ui-invoice.invoice.actions.approve.error.${ERROR_CODES[code]}`,
+        type: 'error',
+      });
+      break;
+    }
+    case ERROR_CODES.budgetExpenseClassNotFound: {
+      const fundCode = error?.errors?.[0]?.parameters?.filter(({ key }) => key === 'fundCode')[0]?.value;
+      const expenseClassName = error?.errors[0]?.parameters?.filter(({ key }) => key === 'expenseClassName')[0]?.value;
+
+      showCallout({ messageId: `ui-invoice.invoice.actions.${action}.error.${code}`, type: 'error', values: { fundCode, expenseClassName } });
+      break;
+    }
+    case ERROR_CODES.inactiveExpenseClass: {
+      const expenseClassId = error?.errors?.[0]?.parameters?.find(({ key }) => key === 'expenseClassId')?.value;
+
+      if (expenseClassId) {
+        expenseClassMutator.GET({ path: `${EXPENSE_CLASSES_API}/${expenseClassId}` })
+          .then(({ name }) => {
+            const values = { expenseClass: name };
+
+            showCallout({
+              messageId: `ui-invoice.invoice.actions.${action}.error.${ERROR_CODES[code]}`,
+              type: 'error',
+              values,
+            });
+          });
+      } else {
+        showCallout({
+          messageId: defaultErrorMessageId,
+          type: 'error',
+        });
+      }
+      break;
+    }
+    default: {
+      showCallout({ messageId: defaultErrorMessageId, type: 'error' });
+    }
+  }
 };
