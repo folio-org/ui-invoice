e8e8a9515afc1da2e0e872094cab76e6463c8a59
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2021-04-23 19:16:09 +0300

    bump commit

--- test/bigtest/network/factories/voucherLine.js
+++ test/bigtest/network/factories/voucherLine.js
@@ -2,7 +2,7 @@ import { Factory } from 'miragejs';
 import faker from 'faker';
 
 export default Factory.extend({
-  id: faker.random.uuid,
+  id: faker.datatype.uuid,
   amount: () => Number.parseFloat(faker.finance.amount(90, 1000, 2)),
   externalAccountNumber: faker.finance.account,
 });
