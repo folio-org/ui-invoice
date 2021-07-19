fed6213418de82d6a7489d8265698ee82226306d
Mikita Siadykh <mikita_siadykh@epam.com>
2021-07-19 09:00:43 +0400

    tests

--- src/invoices/InvoiceForm/InvoiceForm.test.js
+++ src/invoices/InvoiceForm/InvoiceForm.test.js
@@ -2,37 +2,49 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { Form } from 'react-final-form';
 import { act, render, fireEvent } from '@testing-library/react';
+import { useHistory } from 'react-router';
 
-import '@folio/stripes-acq-components/test/jest/__mock__';
+import {
+  HasCommand,
+  expandAllSections,
+  collapseAllSections,
+} from '@folio/stripes/components';
 
 import InvoiceForm from './InvoiceForm';
 
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useHistory: jest.fn(),
+}));
+jest.mock('@folio/stripes-components/lib/Commander', () => ({
+  HasCommand: jest.fn(({ children }) => <div>{children}</div>),
+  expandAllSections: jest.fn(),
+  collapseAllSections: jest.fn(),
+}));
+jest.mock('@folio/stripes-acq-components/lib/AcqUnits/AcqUnitsField', () => {
+  return () => <span>AcqUnitsField</span>;
+});
 jest.mock('../AdjustmentsForm', () => {
   return () => <span>AdjustmentsForm</span>;
 });
 
-jest.mock('@folio/stripes-acq-components/lib/AcqUnits/AcqUnitsField', () => {
-  return () => <span>AcqUnitsField</span>;
-});
-
-const renderInvoiceForm = (
-  initialValues = {},
-  batchGroups = [],
-  parentResources = {},
-) => (render(
+const defaultProps = {
+  initialValues: {},
+  batchGroups: [],
+  parentResources: {},
+  onCancel: jest.fn(),
+};
+const renderInvoiceForm = (props = defaultProps) => (render(
   <MemoryRouter>
     <Form
-      onSubmit={jest.fn}
+      onSubmit={jest.fn()}
       render={() => (
         <InvoiceForm
-          batchGroups={batchGroups}
           form={{}}
-          onSubmit={jest.fn}
-          onCancel={jest.fn}
-          initialValues={initialValues}
+          onSubmit={jest.fn()}
           pristine={false}
           submitting={false}
-          parentResources={parentResources}
+          {...props}
         />
       )}
     />
@@ -40,6 +52,20 @@ const renderInvoiceForm = (
 ));
 
 describe('InvoiceForm component', () => {
+  beforeEach(() => {
+    global.document.createRange = global.document.originalCreateRange;
+  });
+
+  afterEach(() => {
+    global.document.createRange = global.document.mockCreateRange;
+  });
+
+  it('should render correct structure for invoice with batch voucher', () => {
+    const { asFragment } = renderInvoiceForm();
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
   it('should display title', () => {
     const { getByText } = renderInvoiceForm();
 
@@ -85,6 +111,56 @@ describe('InvoiceForm component', () => {
       }));
 
       expect(getByTestId('lock-total-amount')).not.toHaveAttribute('readonly');
+    });
+  });
+
+  describe('Shortcuts', () => {
+    beforeEach(() => {
+      HasCommand.mockClear();
+      expandAllSections.mockClear();
+      collapseAllSections.mockClear();
+    });
+
+    it('should call expandAllSections when expandAllSections shortcut is called', async () => {
+      renderInvoiceForm();
+
+      act(() => {
+        HasCommand.mock.calls[0][0].commands.find(c => c.name === 'expandAllSections').handler();
+      });
+
+      expect(expandAllSections).toHaveBeenCalled();
+    });
+
+    it('should call collapseAllSections when collapseAllSections shortcut is called', () => {
+      renderInvoiceForm();
+
+      act(() => {
+        HasCommand.mock.calls[0][0].commands.find(c => c.name === 'collapseAllSections').handler();
+      });
+
+      expect(collapseAllSections).toHaveBeenCalled();
+    });
+
+    it('should navigate to list when search shortcut is called', () => {
+      const pushMock = jest.fn();
+
+      useHistory.mockClear().mockReturnValue({
+        push: pushMock,
+      });
+
+      renderInvoiceForm();
+      HasCommand.mock.calls[0][0].commands.find(c => c.name === 'search').handler();
+
+      expect(pushMock).toHaveBeenCalledWith('/invoice');
+    });
+
+    it('should navigate close form when cancel shortcut is called', () => {
+      const onCancel = jest.fn();
+
+      renderInvoiceForm({ ...defaultProps, onCancel });
+      HasCommand.mock.calls[0][0].commands.find(c => c.name === 'cancel').handler();
+
+      expect(onCancel).toHaveBeenCalled();
     });
   });
 });
---
+++ src/invoices/InvoiceForm/utils.test.js
@@ -0,0 +1,114 @@
+import { invoice } from '../../../test/jest/fixtures';
+import {
+  saveInvoice,
+} from './utils';
+
+const okapi = {
+  tenant: 'diku',
+  token: 'asd',
+  url: 'folio-okapi',
+};
+
+describe('Invoice form utils', () => {
+  describe('saveInvoice', () => {
+    beforeEach(() => {
+      global.originalFetch = global.fetch;
+
+      global.fetch = jest.fn();
+    });
+
+    afterEach(() => {
+      global.fetch = global.originalFetch;
+    });
+
+    it('should make POST request to save invoice', async () => {
+      const invoiceMutator = {
+        POST: jest.fn().mockReturnValue(invoice),
+      };
+
+      await saveInvoice({ ...invoice, id: null }, [], invoiceMutator, okapi);
+
+      expect(invoiceMutator.POST).toHaveBeenCalled();
+    });
+
+    it('should make PUT request to save invoice', async () => {
+      const invoiceMutator = {
+        PUT: jest.fn().mockReturnValue(invoice),
+      };
+
+      await saveInvoice(invoice, [], invoiceMutator, okapi);
+
+      expect(invoiceMutator.PUT).toHaveBeenCalled();
+    });
+
+    it('should make POST requests to save invoice documents', async () => {
+      const invoiceMutator = {
+        POST: jest.fn().mockReturnValue(invoice),
+      };
+      const invoiceWithDocumnets = {
+        ...invoice,
+        id: null,
+        documents: [{ name: 'Invoice document', data: 'invoice content' }],
+        // links: [{ name: 'Invoice link', url: 'https://folio.com' }],
+      };
+
+      global.fetch.mockClear().mockReturnValue(Promise.resolve());
+
+      await saveInvoice(invoiceWithDocumnets, [], invoiceMutator, okapi);
+
+      expect(fetch).toHaveBeenCalledWith('folio-okapi/invoice/invoices/2e5067cd-2dc8-4d99-900d-b4518bb6407f/documents', {
+        method: 'POST',
+        headers: {
+          'X-Okapi-Tenant': okapi.tenant,
+          'X-Okapi-Token': okapi.token,
+          'Content-Type': 'application/octet-stream',
+        },
+        body: '{"documentMetadata":{"invoiceId":"2e5067cd-2dc8-4d99-900d-b4518bb6407f","name":"Invoice document"},"contents":{"data":"invoice content"}}',
+      });
+    });
+
+    it('should make POST requests to save invoice links', async () => {
+      const invoiceMutator = {
+        POST: jest.fn().mockReturnValue(invoice),
+      };
+      const invoiceWithDocumnets = {
+        ...invoice,
+        id: null,
+        links: [{ name: 'Invoice link', url: 'https://folio.com' }],
+      };
+
+      global.fetch.mockClear().mockReturnValue(Promise.resolve());
+
+      await saveInvoice(invoiceWithDocumnets, [], invoiceMutator, okapi);
+
+      expect(fetch).toHaveBeenCalledWith('folio-okapi/invoice/invoices/2e5067cd-2dc8-4d99-900d-b4518bb6407f/documents', {
+        method: 'POST',
+        headers: {
+          'X-Okapi-Tenant': okapi.tenant,
+          'X-Okapi-Token': okapi.token,
+          'Content-Type': 'application/octet-stream',
+        },
+        body: '{"documentMetadata":{"invoiceId":"2e5067cd-2dc8-4d99-900d-b4518bb6407f","name":"Invoice link","url":"https://folio.com"}}',
+      });
+    });
+
+    it('should make DELETE request to delete invoice document/link', async () => {
+      const invoiceMutator = {
+        PUT: jest.fn().mockReturnValue(invoice),
+      };
+      const prevDocuments = [{ id: 'documentId', invoiceId: invoice.id }];
+
+      global.fetch.mockClear().mockReturnValue(Promise.resolve());
+      await saveInvoice(invoice, prevDocuments, invoiceMutator, okapi);
+
+      expect(fetch).toHaveBeenCalledWith('folio-okapi/invoice/invoices/2e5067cd-2dc8-4d99-900d-b4518bb6407f/documents/documentId', {
+        method: 'DELETE',
+        headers: {
+          'X-Okapi-Tenant': okapi.tenant,
+          'X-Okapi-Token': okapi.token,
+          'Content-Type': 'application/json',
+        },
+      });
+    });
+  });
+});
