0b40ca824539ab14ffcdab188f21e85a948751aa
Yury Saukou <yury_saukou@epam.com>
2023-10-25 17:00:18 +0400

    provide more mock data

--- src/invoices/InvoicesList/ExportSettingsModal/utils/createExportReport.test.js
+++ src/invoices/InvoicesList/ExportSettingsModal/utils/createExportReport.test.js
@@ -19,10 +19,15 @@ import { createExportReport } from './createExportReport';
 const INVALID_REF = 'stripes-acq-components.invalidReference';
 
 const voucher = {
-
+  id: 'voucherId',
+  invoiceId: invoice.id,
+  batchGroupId: invoice.batchGroupId,
 };
 const voucherLine = {
-
+  id: 'voucherLineId',
+  amount: 23.45,
+  voucherId: [voucher.id],
+  externalAccountNumber: '54321098',
 };
 
 const defaultExportData = {
@@ -35,8 +40,6 @@ const defaultExportData = {
   poLineMap: keyBy([orderLine], 'id'),
   userMap: {},
   vendorMap: keyBy([vendor], 'id'),
-  vouchers: [voucher],
-  voucherLines: [voucherLine],
 };
 
 describe('createExportReport', () => {
@@ -46,6 +49,8 @@ describe('createExportReport', () => {
     expect(createExportReport({
       intl: result.current,
       invoiceLines: [invoiceLine],
+      vouchers: [voucher],
+      voucherLines: [voucherLine],
       ...defaultExportData,
     })).toEqual(exportReport);
   });
