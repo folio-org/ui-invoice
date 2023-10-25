b0c529031738d30393111835432a548a4f022142
Yury Saukou <yury_saukou@epam.com>
2023-10-25 16:22:29 +0400

    add test case

--- src/invoices/InvoicesList/ExportSettingsModal/utils/createExportReport.test.js
+++ src/invoices/InvoicesList/ExportSettingsModal/utils/createExportReport.test.js
@@ -1,5 +1,5 @@
 import { useIntl } from 'react-intl';
-import { keyBy } from 'lodash';
+import { keyBy, omit } from 'lodash';
 
 import { renderHook } from '@folio/jest-config-stripes/testing-library/react';
 
@@ -13,23 +13,55 @@ import {
   orderLine,
   vendor,
 } from 'fixtures';
+import { EXPORT_INVOICE_LINE_FIELDS } from '../constants';
 import { createExportReport } from './createExportReport';
 
-test('createExportReport should return export report object', () => {
-  const { result } = renderHook(() => useIntl());
-  const intl = result.current;
+const INVALID_REF = 'stripes-acq-components.invalidReference';
 
-  expect(createExportReport({
-    acqUnitMap: keyBy([acqUnit], 'id'),
-    addressMap: keyBy([address], 'id'),
-    expenseClassMap: {},
-    fiscalYearMap: { [invoice.fiscalYearId]: { code: 'FY2023' } },
-    intl,
-    invoiceLines: [invoiceLine],
-    batchGroupMap: keyBy([batchGroup], 'id'),
-    invoices: [invoice],
-    poLineMap: keyBy([orderLine], 'id'),
-    userMap: {},
-    vendorMap: keyBy([vendor], 'id'),
-  })).toEqual(exportReport);
+const voucher = {
+
+};
+const voucherLine = {
+
+};
+
+const defaultExportData = {
+  acqUnitMap: keyBy([acqUnit], 'id'),
+  addressMap: keyBy([address], 'id'),
+  expenseClassMap: {},
+  fiscalYearMap: { [invoice.fiscalYearId]: { code: 'FY2023' } },
+  batchGroupMap: keyBy([batchGroup], 'id'),
+  invoices: [invoice],
+  poLineMap: keyBy([orderLine], 'id'),
+  userMap: {},
+  vendorMap: keyBy([vendor], 'id'),
+  vouchers: [voucher],
+  voucherLines: [voucherLine],
+};
+
+describe('createExportReport', () => {
+  const { result } = renderHook(() => useIntl());
+
+  it('should return export report object', () => {
+    expect(createExportReport({
+      intl: result.current,
+      invoiceLines: [invoiceLine],
+      ...defaultExportData,
+    })).toEqual(exportReport);
+  });
+
+  it('should display \'invalid reference\' label for broken references', () => {
+    expect(createExportReport({
+      intl: result.current,
+      ...defaultExportData,
+      batchGroupMap: {},
+      vendorMap: {},
+    })).toEqual([expect.objectContaining({
+      ...omit(exportReport[0], Object.keys(EXPORT_INVOICE_LINE_FIELDS)),
+      totalUnits: 0,
+      batchGroup: INVALID_REF,
+      vendorCode: INVALID_REF,
+      vendorAddress: '',
+    })]);
+  });
 });
