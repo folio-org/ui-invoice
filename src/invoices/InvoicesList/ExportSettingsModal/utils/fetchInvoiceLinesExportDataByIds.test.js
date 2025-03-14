817e6bb83c8c25899b338ce5bc3a24fec79281d8
Azizjon Nurov <azizjon_nurov@epam.com>
2025-03-14 11:34:30 +0500

    Add more test

--- src/invoices/InvoicesList/ExportSettingsModal/utils/fetchInvoiceLinesExportDataByIds.test.js
+++ src/invoices/InvoicesList/ExportSettingsModal/utils/fetchInvoiceLinesExportDataByIds.test.js
@@ -1,3 +1,4 @@
+import { INVOICE_LINE_API } from '../../../../common/constants';
 import { fetchInvoiceLinesExportDataByIds } from './fetchInvoiceLinesExportDataByIds';
 
 describe('fetchInvoiceLinesExportDataByIds', () => {
@@ -7,5 +8,15 @@ describe('fetchInvoiceLinesExportDataByIds', () => {
     fetchInvoiceLinesExportDataByIds({ ky, ids: [] });
 
     expect(ky.get).not.toHaveBeenCalled();
+  });
+
+  it('should fetch invoice lines when IDs are provided', async () => {
+    const ky = { get: jest.fn().mockResolvedValue({ json: jest.fn().mockResolvedValue({ invoiceLines: [{ id: '1' }] }) }) };
+
+    const ids = ['1', '2', '3'];
+    const result = await fetchInvoiceLinesExportDataByIds({ ky, ids });
+
+    expect(ky.get).toHaveBeenCalledWith(INVOICE_LINE_API, expect.any(Object));
+    expect(result).toEqual([{ id: '1' }]);
   });
 });
