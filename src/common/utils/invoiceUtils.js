c1052b083a7b10c8e2c7c622725467a15bd06d2e
Azizjon Nurov <azizjon_nurov@epam.com>
2025-03-14 14:57:38 +0500

    Extract fetchInvoiceLines

--- src/common/utils/invoiceUtils.js
+++ src/common/utils/invoiceUtils.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { FolioFormattedDate } from '@folio/stripes-acq-components';
 
 import {
+  INVOICE_LINE_API,
   INVOICE_STATUS,
   POST_APPROVAL_STATUSES,
 } from '../constants';
@@ -22,3 +23,9 @@ export const isPayable = (status) => INVOICE_STATUS.approved === status;
 export const isPaid = (status) => INVOICE_STATUS.paid === status;
 
 export const isCancelled = (status) => INVOICE_STATUS.cancelled === status;
+
+export const fetchInvoiceLines = async (ky, options) => {
+  const { invoiceLines } = await ky.get(INVOICE_LINE_API, { options }).json();
+
+  return invoiceLines;
+};
--- src/invoices/InvoicesList/ExportSettingsModal/utils/fetchInvoiceLinesExportDataByIds.js
+++ src/invoices/InvoicesList/ExportSettingsModal/utils/fetchInvoiceLinesExportDataByIds.js
@@ -5,15 +5,9 @@ import {
   fetchAllRecords,
 } from '@folio/stripes-acq-components';
 
-import { INVOICE_LINE_API } from '../../../../common/constants';
+import { fetchInvoiceLines } from '../../../../common/utils';
 
 const buildInvoiceLinesQuery = (itemsChunk) => itemsChunk.map(id => `invoiceId==${id}`).join(' or ');
-
-const fetchInvoiceLines = async (ky, searchParams) => {
-  const { invoiceLines } = await ky.get(INVOICE_LINE_API, { searchParams }).json();
-
-  return invoiceLines;
-};
 
 export const fetchInvoiceLinesExportDataByIds = ({ ky, ids }) => {
   const batchedIds = chunk(ids, 50);
@@ -22,7 +16,7 @@ export const fetchInvoiceLinesExportDataByIds = ({ ky, ids }) => {
     return acc.then(prevResp => {
       return batchRequest(
         ({ params }) => fetchAllRecords({
-          GET: async ({ params: searchParams }) => fetchInvoiceLines(ky, searchParams),
+          GET: async ({ params: options }) => fetchInvoiceLines(ky, options),
         }, params.query),
         nextBatch,
         buildInvoiceLinesQuery,
