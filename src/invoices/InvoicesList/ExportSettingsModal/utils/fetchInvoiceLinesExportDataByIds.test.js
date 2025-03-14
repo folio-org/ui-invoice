5fc8eedda4159dc4c1fc9929c58ed719c1ca4942
Azizjon Nurov <azizjon_nurov@epam.com>
2025-03-14 14:16:42 +0500

    Remove test

--- src/invoices/InvoicesList/ExportSettingsModal/utils/fetchInvoiceLinesExportDataByIds.test.js
+++ src/invoices/InvoicesList/ExportSettingsModal/utils/fetchInvoiceLinesExportDataByIds.test.js
@@ -1,4 +1,3 @@
-import { INVOICE_LINE_API } from '../../../../common/constants';
 import { fetchInvoiceLinesExportDataByIds } from './fetchInvoiceLinesExportDataByIds';
 
 describe('fetchInvoiceLinesExportDataByIds', () => {
@@ -8,15 +7,5 @@ describe('fetchInvoiceLinesExportDataByIds', () => {
     fetchInvoiceLinesExportDataByIds({ ky, ids: [] });
 
     expect(ky.get).not.toHaveBeenCalled();
-  });
-
-  it('should fetch invoice lines when IDs are provided', async () => {
-    const ky = { get: jest.fn().mockResolvedValue({ invoiceLines: [{ id: '1' }] }) };
-
-    const ids = ['1', '2', '3'];
-    const result = await fetchInvoiceLinesExportDataByIds({ ky, ids });
-
-    expect(ky.get).toHaveBeenCalledWith(INVOICE_LINE_API, expect.any(Object));
-    expect(result).toEqual([{ id: '1' }]);
   });
 });
